e88c0fc9467df7c920d676562234e9f602709072
carlschader <carlschader@gmail.com>
2021-08-08 15:26:15 -0600

    adjusted codeblock font size on mobile

--- components/CodeBlock.js
+++ components/CodeBlock.js
@@ -1,16 +1,29 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula as syntaxStyle } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import Typography from '@material-ui/core/Typography';
+import Responsive from './Responsive';
 
 export default function CodeBlock({ children, className }) {
-    const language = /language-(\w+)/.exec(className || '')[1];
+  const language = /language-(\w+)/.exec(className || '')[1];
 
-    return (
+  return (
+    <Responsive
+      desktop={
         <div style={{ textAlign: 'left', fontSize: '1rem' }}>
-            <Typography>{language}</Typography>
-            <SyntaxHighlighter language={language} style={syntaxStyle} >
-                {children}
-            </SyntaxHighlighter>
+          <Typography>{language}</Typography>
+          <SyntaxHighlighter language={language} style={syntaxStyle} >
+            {children}
+          </SyntaxHighlighter>
         </div>
-    );
+      }
+      mobile={
+        <div style={{ textAlign: 'left', fontSize: '0.67rem' }}>
+          <Typography>{language}</Typography>
+          <SyntaxHighlighter language={language} style={syntaxStyle} >
+            {children}
+          </SyntaxHighlighter>
+        </div>
+      }
+    />
+  );
 }
