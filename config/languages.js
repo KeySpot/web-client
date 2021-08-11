97fb5233cb1b520dfec0f7a4ee0d19516c536fbc
carlschader <carlschader@gmail.com>
2021-08-10 18:38:26 -0600

    go added to packages

--- config/languages.js
+++ config/languages.js
@@ -1,11 +1,12 @@
 import { IconContext } from "react-icons";
 import { DiNodejsSmall, DiPython } from 'react-icons/di';
+import { Golang } from 'grommet-icons';
 
 const style = {
     size: '3em',
 };
 
-function wrapIcon(Icon) {
+function wrapIcon(Icon, style) {
     return (
         <IconContext.Provider value={style} >
             <Icon />
@@ -16,10 +17,14 @@ function wrapIcon(Icon) {
 export default {
     nodejs: {
         url: "https://github.com/KeySpot/node-api",
-        icon: wrapIcon(DiNodejsSmall),
+        icon: wrapIcon(DiNodejsSmall, style),
     },
     python: {
         url: "https://github.com/KeySpot/python-api",
-        icon: wrapIcon(DiPython),
+        icon: wrapIcon(DiPython, style),
+    },
+    go: {
+        url: "https://github.com/keyspot/gopackage",
+        icon: <Golang color="white" size="40px" />, 
     },
 };
