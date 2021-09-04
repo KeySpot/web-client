773119561a0f6d1994a97ef6c98511b20a412217
carlschader <carlschader@gmail.com>
2021-09-04 07:57:42 -0600

    added google analytics tracking

--- components/HtmlBase.js
+++ components/HtmlBase.js
@@ -1,6 +1,9 @@
+import process from 'process';
 import Head from 'next/head';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from './AppBar';
+
+const googleAnalyticsId = process.env.GOOGLE_ANALYTICS_ID;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,6 +22,16 @@ export default function HtmlBase({ children = [], title = "KeySpot" }) {
         <title>{title}</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-205481979-1">
+        </script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){'{'}dataLayer.push(arguments);{'}'}
+          gtag(&apos;js&apos;, new Date());
+
+          gtag(&apos;config&apos;, &apos;{googleAnalyticsId}&apos;);
+        </script>
       </Head>
 
       <main className="main">
